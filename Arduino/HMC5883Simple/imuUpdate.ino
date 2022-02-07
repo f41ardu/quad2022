8da1596b458015705f5df8e48eac5f694163dd55
f41ardu <f41thr@arcor.de>
2022-02-07 18:16:18 +0100

    HMC5883Simple folder removed

--- Arduino/HMC5883Simple/imuUpdate.ino
+++
@@ -1,5 +0,0 @@
-void quaternionToRollPitchYaw() {
-  roll  = atan2((q[0] * q[1] + q[2] * q[3]), 0.5 - (q[1] * q[1] + q[2] * q[2]));
-  pitch = asin(2.0 * (q[0] * q[2] - q[1] * q[3]));
-  yaw   = atan2((q[1] * q[2] + q[0] * q[3]), 0.5 - ( q[2] * q[2] + q[3] * q[3]));
-}      
